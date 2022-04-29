e32e937c8f21e58d0a35313aa788575ecfbc7088
André de Sousa Lacerda <a.de.sousa.lacerda@avanade.com>
2022-04-29 11:28:48 -0300

    nova branch

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ const sumWithInitial = array1.reduce(
 
 console.log(sumWithInitial);
 // expected output: 10
+
+
+var num1 = [1, 2, 3];
+var num2 = [4, 5, 6];
+var num3 = [7, 8, 9];
+
+var nums = num1.concat(num2, num3);
+console.log(nums);
+
+
